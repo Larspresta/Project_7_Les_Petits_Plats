32c2c4f528cc1a4dd1e39da8d65fdd36f7968386
LarsPresta <lauri.monni@gmail.com>
2024-01-03 23:19:00 +0100

    factory

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ import { recipes } from "./recipes.js";
 
 const recipeContainer = document.querySelector("[data-recipe-card-container]");
 const recipeCard = document.querySelector("[data-recipe-card]");
+const searchInput = document.querySelector("[data-search]");
+
+searchInput.addEventListener("input", (e) => {
+  const value = e.target.value;
+  console.log(value);
+});
 
 console.log("here is the recipes array", recipes);
 
@@ -10,8 +16,26 @@ recipes.forEach((recipe) => {
   const title = card.querySelector("[data-card-title]");
   const time = card.querySelector("[data-card-time]");
   const description = card.querySelector("[data-card-description]");
+  const ingredients = card.querySelector("[data-card-ingredients]");
   title.textContent = recipe.name;
   time.textContent = recipe.time;
   description.textContent = recipe.description;
+
+  recipe.ingredients.forEach((ingredient) => {
+    const ingredientItem = document.createElement("li");
+
+    const ingredientItemName = document.createElement("span");
+    ingredientItemName.classList.add("fw-bold");
+    const ingredientValueUnit = document.createElement("span");
+
+    ingredientItemName.textContent = `${ingredient.ingredient}:`;
+
+    ingredientValueUnit.textContent = ` ${ingredient.quantity || ""} ${ingredient.unit || ""}`;
+
+    ingredientItem.appendChild(ingredientItemName);
+    ingredientItem.appendChild(ingredientValueUnit);
+    ingredients.appendChild(ingredientItem);
+  });
+
   recipeContainer.append(card);
 });
